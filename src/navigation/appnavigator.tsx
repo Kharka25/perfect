f2bb1427eef8fa2daa9e26d0acafd5e82903fb8a
Kharka <adewoyindaniel6@gmail.com>
2024-11-06 23:52:40 +0000

    tab icons

--- src/navigation/appnavigator.tsx
+++ src/navigation/appnavigator.tsx
@@ -2,16 +2,28 @@ import React from 'react';
 
 import GenerateNavigator from '@config/generatenavigator';
 import {ScreenType} from '@models/navigation';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
 import {Explore, Home} from '@screens';
 
 const tabs: ScreenType[] = [
   {
     name: 'Home',
     component: Home,
+    options: {
+      tabBarIcon: props => {
+        return <Icon name="home" color={props.color} size={20} />;
+      },
+    },
   },
   {
     name: 'Explore',
     component: Explore,
+    options: {
+      tabBarIcon: props => {
+        return <Icon name="explore" color={props.color} size={20} />;
+      },
+    },
   },
 ];
 
